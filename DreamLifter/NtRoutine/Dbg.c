3a731e8ba43297619ee590d695dc03a571a7b9aa
Bingxing Wang <i@imbushuo.net>
2020-08-16 01:40:40 -0400

    Dbg: redirect DbgPrint output to console and VS debug window

--- DreamLifter/NtRoutine/Dbg.c
+++ DreamLifter/NtRoutine/Dbg.c
@@ -8,11 +8,17 @@ ULONG DlkDbgPrint(
 )
 {
 	ULONG i = 0;
+	char DebugBuffer[512];
 
 	va_list Args;
 	va_start(Args, Format);
-	i = vprintf(Format, Args);
-	va_end(Args);
 
+	i = vprintf(Format, Args);
+	if (sprintf_s(DebugBuffer, 512, Format, Args))
+	{
+		OutputDebugStringA(DebugBuffer);
+	}
+
+	va_end(Args);
 	return i;
 }
